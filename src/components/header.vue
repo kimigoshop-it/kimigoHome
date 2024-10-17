8d193102fa4c6b6ff1d21de6348fcb2340203446
加藤恵 <q2569628572@gmail.com>
2024-10-17 10:16:01 +0800

    调整移动端模式导航

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="headers">
+    <div
+        class="headers"
+        :class="{
+            'menu-expanded': menuShow,
+        }"
+    >
         <header class="headerTop w">
             <img :src="logo" />
             <div class="routesty">
@@ -18,18 +23,44 @@
                 <div class="select">
                     <LanguageChange />
                 </div>
+                <div
+                    class="menu-container"
+                    :class="{
+                        'menu-active': menuShow,
+                    }"
+                    @click="menuShow = !menuShow"
+                >
+                    <div class="menu-item">
+                        <div class="menu-sub__item"></div>
+                    </div>
+                    <div class="menu-item">
+                        <div class="menu-sub__item"></div>
+                    </div>
+                </div>
             </div>
         </header>
+        <div class="menu-wrapper">
+            <router-link
+                v-for="(item, index) in routerdata"
+                :key="item.id"
+                :to="item.path"
+                :class="active === index ? 'bott' : ''"
+                :style="{ borderBottom: routerjjfaFlag ? 'none' : '' }"
+                @click="clickactive(index)"
+            >
+                {{ item.name }}
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { useI18n } from "vue-i18n";
-const { t } = useI18n();
-import LanguageChange from "./lang/LanguageChange.vue";
-import { ref, reactive, computed } from "vue";
-import { useRoute, useRouter } from "vue-router";
 import logo from "@/assets/image/logo.png";
+import { computed, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { useRoute, useRouter } from "vue-router";
+import LanguageChange from "./lang/LanguageChange.vue";
+const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
 let routerjjfaFlag = ref(false);
@@ -50,6 +81,7 @@ const options = ref([
     { name: "哈文", id: 1 },
     { name: "俄语", id: 2 },
 ]);
+const menuShow = ref(false);
 const routerdata = computed(() => {
     return [
         { path: "#home", name: t("home.pickup.pickup"), id: 0 },
@@ -95,6 +127,7 @@ onUnmounted(() => {
     window.removeEventListener("scroll", handleScroll);
 });
 const clickactive = (index: any) => {
+    menuShow.value = false;
     const pathList = [
         "/home/index#home",
         "/home/index#jjfa",
@@ -193,6 +226,17 @@ a:focus {
 }
 
 @media (max-width: 999px) {
+    .headers {
+        height: 72px;
+        transition: height 0.5s ease-in-out;
+    }
+
+    .menu-wrapper {
+        height: 0px;
+        overflow: hidden;
+        transition: height 0.5s ease-in-out;
+    }
+
     header {
         align-items: center;
         position: relative;
@@ -200,42 +244,111 @@ a:focus {
         padding-inline-end: 1.5rem;
     }
 
+    .menu-expanded {
+        height: 100vh;
+    }
+
+    .menu-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+
+    .menu-expanded .menu-wrapper {
+        height: 100%;
+    }
+
+    .menu-wrapper .router-link-active {
+        margin: 0px 24px;
+        padding: 12px 0px;
+    }
+
     .headers .headerTop img {
         margin-top: inherit;
     }
 
     header .routesty > div:nth-child(1) {
-        // display: none;
-        padding: 0.75rem 0;
-        margin-right: 0;
-        border-radius: 1rem;
-        overflow: hidden;
-        display: flex;
-        justify-content: space-around;
-        position: fixed;
-        left: 50%;
-        bottom: 1.5rem;
-        transform: translateX(-50%);
-        background-color: hsla(0, 0%, 100%, 0.9);
-        backdrop-filter: saturate(60%) blur(20px);
-        box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
-
-        width: calc(100% - 3rem);
-    }
-
-    header .routesty > div:nth-child(1) > a {
-        margin-left: 0;
-        padding-bottom: 0;
-        white-space: nowrap;
+        display: none;
     }
 
     header .routesty {
+        align-items: center;
         margin-top: inherit;
-        flex-direction: column-reverse;
     }
 
     .select {
         width: fit-content;
+    }
+
+    .menu-container {
+        width: 44px;
+        height: 44px;
+        position: relative;
+        margin-left: 12px;
+    }
+
+    .menu-item {
+        height: 20px;
+        left: 13px;
+        pointer-events: none;
+        position: absolute;
+        top: 12px;
+        transition: transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96);
+        width: 20px;
+        z-index: 1;
+    }
+
+    .menu-sub__item {
+        background-color: black;
+        border-radius: 1px;
+        display: block;
+        height: 2px;
+        position: absolute;
+        transition: transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84)
+            0.1008s;
+        width: 20px;
+        z-index: 1;
+    }
+
+    .menu-container .menu-item:first-child .menu-sub__item {
+        top: 9px;
+        transform: translateY(-4px);
+    }
+
+    .menu-container .menu-item:last-child .menu-sub__item {
+        bottom: 9px;
+        transform: translateY(4px);
+    }
+
+    .menu-container.menu-active .menu-item {
+        height: 24px;
+        left: 10px;
+        top: 11px;
+        transition: transform 0.3192s cubic-bezier(0.04, 0.04, 0.12, 0.96)
+            0.1008s;
+        width: 24px;
+    }
+
+    .menu-container.menu-active .menu-item:first-child {
+        transform: rotate(-45deg);
+    }
+
+    .menu-container.menu-active .menu-item:last-child {
+        transform: rotate(45deg);
+    }
+
+    .menu-container.menu-active .menu-item .menu-sub__item {
+        transform: translateY(0);
+        transition: transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96);
+        width: 24px;
+    }
+
+    .menu-container.menu-active .menu-item:first-child .menu-sub__item {
+        top: 11px;
+    }
+
+    .menu-container.menu-active .menu-item:last-child .menu-sub__item {
+        bottom: 11px;
     }
 }
 </style>
